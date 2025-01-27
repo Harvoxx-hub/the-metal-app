16ab2d726b627b3614292ac1c1952a7bd4f98e3e
Favour williams <90847660+hello-favour@users.noreply.github.com>
2025-01-27 13:39:49 +0100

    resolved user login auth error message

--- lib/features/authentication/provider/login.notifier.dart
+++ lib/features/authentication/provider/login.notifier.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:metal/core/state/base.state.dart';
 import 'package:metal/features/authentication/data/repositories/authetication.repository.dart';
@@ -11,6 +12,40 @@ class LoginNotifier extends StateNotifier<LoginStates> {
     this.ref,
   );
   final Ref ref;
+//   void login({
+//     required String email,
+//     required String password,
+//   }) async {
+//     state = LoginStates.loading();
+//     debugPrint("LOGIN STATE:$state");
+//     try {
+//       final authenticationRepository =
+//           ref.watch(authenticationRepositoryProvider);
+//       final response = await authenticationRepository.logIn(
+//         email: email,
+//         password: password,
+//       );
+
+//       if (response.success!) {
+//         final Map<String, dynamic> data = response.data;
+//         ref
+//             .read(authProvider.notifier)
+//             .updateUserData(UserModel.fromJson(data));
+
+//         state = LoginStates.success(UserModel.fromJson(data));
+//         debugPrint("LOGIN RESPONSE SUCCESS:${response.success}");
+//       } else {
+//         debugPrint("LOGIN ERROR RESPONSE:$response");
+//         state = LoginStates.error(
+//           response.message!,
+//         );
+//       }
+//     } catch (e, s) {
+//       debugPrint('LOGIN Error: $e, LOGIN StackTrace: $s');
+//       state = LoginStates.error(e.toString(), stackTrace: s);
+//     }
+//   }
+// }
 
   //login user
   void login({
@@ -32,19 +67,39 @@ class LoginNotifier extends StateNotifier<LoginStates> {
         ref
             .read(authProvider.notifier)
             .updateUserData(UserModel.fromJson(data));
-        
-           
+
         state = LoginStates.success(UserModel.fromJson(data));
         debugPrint("LOGIN RESPONSE SUCCESS:${response.success}");
       } else {
         debugPrint("LOGIN ERROR RESPONSE:$response");
         state = LoginStates.error(
-          response.message!,
+          "The email or password you entered is incorrect. Please try again.",
         );
       }
     } catch (e, s) {
       debugPrint('LOGIN Error: $e, LOGIN StackTrace: $s');
-      state = LoginStates.error(e.toString(), stackTrace: s);
+
+      if (e is PlatformException && e.code == 'ERROR_INVALID_CREDENTIAL') {
+        state = LoginStates.error(
+          "The email or password you entered is incorrect. Please try again.",
+          stackTrace: s,
+        );
+      } else if (e is PlatformException && e.code == 'ERROR_USER_NOT_FOUND') {
+        state = LoginStates.error(
+          "No account found with this email address. Please check your email or sign up for a new account.",
+          stackTrace: s,
+        );
+      } else if (e is PlatformException && e.code == 'ERROR_WRONG_PASSWORD') {
+        state = LoginStates.error(
+          "The password you entered is incorrect. Please try again.",
+          stackTrace: s,
+        );
+      } else {
+        state = LoginStates.error(
+          "An unexpected error occurred. Please try again later.",
+          stackTrace: s,
+        );
+      }
     }
   }
 }
